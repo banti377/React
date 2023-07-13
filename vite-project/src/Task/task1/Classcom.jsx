6dbbb2e9bcd32dcb3f3c9e0064c1df10e6011fd8
Banti Patel <bantipatel2506@gmail.com>
2023-07-13 14:37:49 +0530

    work done

---
+++ vite-project/src/Task/task1/Classcom.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+
+class ClassCom extends React.Component {
+    render() {
+        return <div>
+            <div>
+                Header
+            </div>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact us</li>
+                </ul>
+            </nav>
+            <div>
+                Main Content
+            </div>
+            <footer>
+                Footer
+            </footer>
+        </div>
+    }
+}
+
+export default ClassCom
---
+++ vite-project/src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import ClassCom from './Task/task1/Classcom.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <ClassCom />
+  </React.StrictMode>,
+)
