6028ab21a9617c59b811f498a4e34ca453c174d8
Banti Patel <bantipatel2506@gmail.com>
2023-07-13 15:28:43 +0530

    done

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import ClassCom from './Task/task1/Classcom.jsx'
+import Fun from './Task/task1/Fun.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
     <ClassCom />
+    <Fun />
   </React.StrictMode>,
 )
