244d4cabbab261e84f9399d37b7f7ecc7c99e5cc
Banti Patel <bantipatel2506@gmail.com>
2023-07-20 10:52:19 +0530

    add work

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,27 +1,43 @@
 import React, { useState } from 'react'
 import './App.css'
-
-export default function FunState() {
-  let [count, setCount] = useState(0);
-  let x = 0;
+import ListKey from './List/ListKey'
 
 
-  function updateCount(params) {
-    x++
-    console.log(x);
-  }
-  function updateState(params) {
-    setCount((pre) => pre + 1)
-    console.log(count);
-  }
-
+function App() {
   return (
-    < div >
-      <h1>{x}</h1>
-      <button onClick={() => updateCount()}>Count Inc</button>
-      <hr />
-      <h1>{count}</h1>
-      <button onClick={() => updateState()}>Count Inc</button>
-    </div >
+    <div>
+      <ListKey />
+    </div>
   )
+
 }
+
+export default App;
+
+
+
+// export default function FunState() {
+//   let [count, setCount] = useState(0);
+//   let x = 0;
+
+
+//   function updateCount(params) {
+//     x++
+//     console.log(x);
+//   }
+//   function updateState(params) {
+//     setCount((pre) => pre + 1)
+//     console.log(count);
+//   }
+
+//   return (
+//     < div >
+//       <h1>{x}</h1>
+//       <button onClick={() => updateCount()}>Count Inc</button>
+//       <hr />
+//       <h1>{count}</h1>
+//       <button onClick={() => updateState()}>Count Inc</button>
+//     </div >
+//   )
+// }
+
---
+++ vite-project/src/List/ListKey.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function ListKey() {
+    let arr = ["banti", "vivek", "axit", "manish"]
+
+    function onclick() {
+        console.log("---------->");
+    }
+    return (
+        <>
+            <ul>
+                {arr.map((e, i) => {
+                    return (
+                        <li key={i} onClick={() => onclick()}>
+                            {e}
+                        </li>
+                    )
+                })}
+            </ul >
+        </>
+    )
+}
