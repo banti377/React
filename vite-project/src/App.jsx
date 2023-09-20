3b8c544c4c1ff8e5e9fe7868e50fd385029ae492
banti377 <bantipatel2506@gmail.com>
2023-09-20 13:46:30 +0530

    add data

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,25 +1,24 @@
-import React, { useState } from 'react'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
-import Fun from './1.component/Func';
-import ListKey from './8.List/ListKey';
-import Test from './6.map/Test';
-import InputCom from './9.input/InputCom';
-import InputTask from './9.input/InputTask';
-import ClassCom from './1.component/Classcom';
-import MultipleInputCom from './9.input/MultipleInputCom';
-import MultipleInputTask from './9.input/MultipleInputTask';
-import RegForm from './9.input/RegForm';
-import StopWatch from './stopwatch/StopWatch';
-import App2 from './18.Redux/App2';
-
+import Fun from "./1.component/Func";
+import ListKey from "./8.List/ListKey";
+import Test from "./6.map/Test";
+import InputCom from "./9.input/InputCom";
+import InputTask from "./9.input/InputTask";
+import ClassCom from "./1.component/Classcom";
+import MultipleInputCom from "./9.input/MultipleInputCom";
+import MultipleInputTask from "./9.input/MultipleInputTask";
+import RegForm from "./9.input/RegForm";
+import StopWatch from "./stopwatch/StopWatch";
+import App2 from "./18.Redux/App2";
 
 function App() {
   return (
     <div>
       <App2 />
       {/* <StopWatch /> */}
-      {/* <RegForm /> */}
+      <RegForm />
       {/* <MultipleInputTask /> */}
       {/* <MultipleInputCom /> */}
       {/* <InputTask /> */}
@@ -29,18 +28,14 @@ function App() {
       {/* <Fun /> */}
       {/* <ListKey /> */}
     </div>
-  )
-
+  );
 }
 
 export default App;
 
-
-
 // export default function FunState() {
 //   let [count, setCount] = useState(0);
 //   let x = 0;
-
 
 //   function updateCount(params) {
 //     x++
@@ -61,4 +56,3 @@ export default App;
 //     </div >
 //   )
 // }
-
