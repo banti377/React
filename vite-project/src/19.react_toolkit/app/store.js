8e3cecdf1f89e432b48f35006ad65989c7e73c84
banti377 <bantipatel2506@gmail.com>
2023-09-22 17:10:25 +0530

    add data

--- vite-project/src/19.react_toolkit/app/store.js
+++ vite-project/src/19.react_toolkit/app/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import countReducer from "../fetures/count/countSlice"
+import countReducer from "../fetures/count/countSlice";
 
 export const store = configureStore({
-    reducer: {
-        COUNT: countReducer,
-    }
-})
+  reducer: {
+    COUNT: countReducer,
+  },
+});
