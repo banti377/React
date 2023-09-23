d830769c42b2b37d3cbcfa808b572e09c87eb73f
banti377 <bantipatel2506@gmail.com>
2023-09-23 13:23:36 +0530

    add data

--- vite-project/src/19.react_toolkit/AppToolKit.jsx
+++ vite-project/src/19.react_toolkit/AppToolKit.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { Provider } from 'react-redux'
-import CountCom from './CountCom'
-import { store } from './app/store'
+import React from "react";
+import { Provider } from "react-redux";
+import CountCom from "./CountCom";
+import { store } from "./app/store";
 
 export default function Apptoolkit() {
-    return (
-        <>
-            <Provider store={store}>
-                <CountCom />
-            </Provider>
-        </>
-    )
+  return (
+    <>
+      <Provider store={store}>
+        <CountCom />
+      </Provider>
+    </>
+  );
 }
